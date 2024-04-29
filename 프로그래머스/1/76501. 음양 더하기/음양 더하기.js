9f8fe22de73844275536d8abdffd78bf0e004eca
sangjae99 <166293153+sangjae99@users.noreply.github.com>
2024-04-29 15:38:29 +0900

    [level 1] Title: 음양 더하기, Time: 0.15 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -4,7 +4,7 @@ function solution(absolutes, signs) {
 }function solution(absolutes, signs) {
     let sum = 0;
     for (let i = 0; i < absolutes.length; i++) {
-        // signs[i]가 true이면 양수, false이면 음수로 처리하여 더함
+      
         sum += signs[i] ? absolutes[i] : -absolutes[i];
     }
     return sum;
