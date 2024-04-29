fe433890fc96db507fb333a9909918bc52141f5d
sangjae99 <166293153+sangjae99@users.noreply.github.com>
2024-04-29 15:34:59 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,31 +1,20 @@
 function solution(s){
     var answer = true;
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    console.log('Hello Javascript')
-
-    return answer;
-}function solution(s) {
-    // 소문자로 변환하여 비교하기 쉽게 함
-    s = s.toLowerCase();
+    var pcount = 0;
+    var ycount = 0;
     
-    // 'p'와 'y'의 개수를 담을 변수 초기화
-    let pCount = 0;
-    let yCount = 0;
-    
-    // 문자열을 순회하면서 'p'와 'y'를 찾음
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === 'p') {
-            pCount++;
-        } else if (s[i] === 'y') {
-            yCount++;
-        }
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] === 'p' || s[i] === 'P') {
+            pcount++;
+        } else if(s[i] === 'y' || s[i] === 'Y') {
+            ycount++;
+        } 
     }
+    if(pcount == ycount){
+        answer = true;
+    } else {
+        answer = false;
+    }
+    return answer
     
-    // 'p'와 'y'의 개수가 같으면 true, 아니면 false 반환
-    return pCount === yCount;
 }
-
-// 테스트
-console.log(solution("pPoooyY")); // 예시 입력: true
-console.log(solution("Pyy")); // 예시 입력: false
