d1fb7409d2d08213f98bbd97ccfc3e7fa8a3e826
sangjae99 <166293153+sangjae99@users.noreply.github.com>
2024-04-29 15:47:37 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,15 +1,15 @@
 function solution(absolutes, signs) {
-    var answer = 123456789;
-    return answer;
-}function solution(absolutes, signs) {
-    let sum = 0;
+    let answer = 0;
+    
     for (let i = 0; i < absolutes.length; i++) {
+        if (signs[i] === true) {
+            answer += absolutes[i]
+        } else {
+            answer -= absolutes[i]
+        }
       
-        sum += signs[i] ? absolutes[i] : -absolutes[i];
     }
-    return sum;
+    return answer
 }
 
-// 테스트
-console.log(solution([4, 7, 12], [true, false, true])); // 예시 입력: 9
-console.log(solution([1, 2, 3], [false, false, true])); // 예시 입력: 0
+
